9f0e4d77eb4d3f55a489f987aa1479dedc07337d
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2024-05-14 14:08:53 +0600

    created NextJs app and created basic folder structure

---
+++ src/components/shared/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div>
+            <h2 className="text-green-500 text-xl font-bold text-center">From footer</h2>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/shared/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2 className="text-red-500 text-xl font-bold text-center">From Navbar</h2>
+        </div>
+    );
+};
+
+export default Navbar;
