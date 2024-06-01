70e3d18c4e3c169c75f8ac61696bae78f7f28f9c
Himel <khondokarh55@gmail.com>
2024-06-01 17:46:40 +0600

    updated

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+"use client"
+import { useEffect, useState } from 'react';
+
+interface singleProductData {
+    product_id : string
+}
 
 const Page = ({params} : {params: {productId:string}}) => {
+    const [products, setProducts] = useState([]);
+    const [singleProduct,setSingleProduct] = useState()
+
+    useEffect(() => {
+        fetch('/productData/productData.json')
+          .then(response => response.json())
+          .then(data => setProducts(data))
+          .catch(error => console.error('Error fetching products:', error));
+          
+      }, []);
+    
+    useEffect(() => {
+        if(products.length > 0){
+            const singleProduct = products.find( (p : singleProductData) => p.product_id === params.productId)
+            setSingleProduct(singleProduct)
+        }
+        console.log(singleProduct);
+    },[products , params.productId , singleProduct])
+   
+
+
+    console.log(singleProduct);
+    console.log(products);
+
+   
+
     return (
         <div>
-            <h1 className='text-4xl'>details about {params.productId}</h1>
+            <h1 className='text-4xl'>details about</h1>
         </div>
     );
 }
---
+++ src/components/pages/DetailsPage/productImage.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from "react";
+import { Image } from "antd";
+
+const ProductImage = () => {
+  return (
+    <div>
+      <Image
+        alt="flower"
+        width={200}
+        src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+      />
+    </div>
+  );
+};
+
+export default ProductImage;
