e1dd1d291b22983d4cd4a5cb24b700303f9ac5a6
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2024-06-23 18:34:36 +0600

    updated

--- src/app/api/product/all-products/route.ts
+++ src/app/api/product/all-products/route.ts
@@ -2,8 +2,8 @@ import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const prisma = new PrismaClient();
     try {
+        const prisma = new PrismaClient();
         const result = await prisma.products.findMany();
         return NextResponse.json({ status: "success", data: result });
     } catch (error) {
