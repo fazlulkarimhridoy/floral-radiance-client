498817bd033295e47bb464ddc34f36ec7f4bbacd
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2024-11-11 00:37:38 +0600

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "res.cloudinary.com",
+                hostname: "backend.floralradiancebd.com",
             },
         ],
     },
