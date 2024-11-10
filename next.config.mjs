4e0dd551ac1a01195b302036c5d19974f14c5b03
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2024-11-10 15:24:13 +0600

    updated cloudnary

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@
 const nextConfig = {
     output: "standalone",
     reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
